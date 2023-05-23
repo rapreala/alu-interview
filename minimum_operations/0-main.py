e27ca76f654e3fdece242c86ecee8e8a035bae2b
Ryan Apreala <r.apreala@alustudent.com>
2023-05-23 18:01:32 +0000

    push

--- minimum_operations/0-main.py
+++ minimum_operations/0-main.py
@@ -2,11 +2,11 @@
 """
 Main file for testing
 """
-
 from minoperations import minOperations
 
 n = 4
 print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
 
 n = 12
 print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
