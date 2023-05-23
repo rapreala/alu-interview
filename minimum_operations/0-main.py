8ef7088dc32ebff9ba4d9cfde18718b8ef7d99d5
Ryan Apreala <r.apreala@alustudent.com>
2023-05-23 17:59:52 +0000

    push

---
+++ minimum_operations/0-main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Main file for testing
+"""
+
+from minoperations import minOperations
+
+n = 4
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
+n = 12
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
---
+++ minimum_operations/0-minoperations.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Minimum Operations
+
+This module contains a function that calculates the fewest number of operations needed to result in exactly n H characters.
+
+"""
+
+def minOperations(n):
+    """
+    Calculates the fewest number of operations needed to result in exactly n H characters.
+
+    Args:
+        n (int): The target number of H characters.
+
+    Returns:
+        int: The minimum number of operations.
+
+    """
+    if n <= 1:
+        return 0
+    factors = []
+    divisor = 2
+    while n > 1:
+        if n % divisor == 0:
+            factors.append(divisor)
+            n //= divisor
+        else:
+            divisor += 1
+    return sum(factors)
